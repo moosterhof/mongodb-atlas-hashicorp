3927fbb555fccbb4889aba7cc21df9b219d325a4
Michel Oosterhof <moosterhof@hashicorp.com>
2021-03-11 15:44:02 +0800

    remove duplicate code

--- main.tf
+++ main.tf
@@ -1,20 +1,3 @@
-terraform {
-    required_providers {
-        mongodbatlas = {
-            source = "mongodb/mongodbatlas"
-        }
-    }
-}
-
-# Configure the MongoDB Atlas Provider
-provider "mongodbatlas" {
-  public_key = var.mongodbatlas_public_key
-  private_key  = var.mongodbatlas_private_key
-}
-
-#Create the resources
-# ...
-
 
 output "atlasclusterstring" {
     value = mongodbatlas_cluster.cluster-atlas.connection_strings
