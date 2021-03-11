ed55401ce5f04425bc4836176ead0f29f8cd2288
Michel Oosterhof <moosterhof@hashicorp.com>
2021-03-11 15:36:01 +0800

    add maintf with mongodbatlas provider

---
+++ main.tf
@@ -0,0 +1,16 @@
+terraform {
+    required_providers {
+        mongodbatlas = {
+            source = "mongodb/mongodbatlas"
+        }
+    }
+}
+
+# Configure the MongoDB Atlas Provider
+provider "mongodbatlas" {
+  public_key = var.mongodbatlas_public_key
+  private_key  = var.mongodbatlas_private_key
+}
+
+#Create the resources
+# ...
