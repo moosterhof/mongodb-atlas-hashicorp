1b610ac8c78a896e11e124be3b0f43715dc4559d
Michel Oosterhof <moosterhof@hashicorp.com>
2021-03-11 15:40:45 +0800

    add provider

---
+++ provider.tf
@@ -0,0 +1,18 @@
+terraform {
+    required_providers {
+        mongodbatlas = {
+            source = "mongodb/mongodbatlas"
+        }
+    }
+}
+
+provider "mongodbatlas" {
+  public_key  = var.mongodb_public_key
+  private_key = var.mongodb_private_key
+}
+
+provider "aws" {
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region     = var.aws_region
+}
