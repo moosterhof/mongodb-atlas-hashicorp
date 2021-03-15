cf7f915c989d65cdaa37818f42d7997707e201ae
Michel Oosterhof <moosterhof@hashicorp.com>
2021-03-15 17:30:29 +0800

    comment out bits that don't work

--- atlas-cluster.tf
+++ atlas-cluster.tf
@@ -19,6 +19,8 @@ resource "mongodbatlas_cluster" "cluster-atlas" {
 output "atlasclusterstring" {
   value = mongodbatlas_cluster.cluster-atlas.connection_strings
 }
-output "plstring" {
-  value = lookup(mongodbatlas_cluster.cluster-atlas.connection_strings[0].aws_private_link_srv, aws_vpc_endpoint.ptfe_service.id)
-}
+
+# TODO: fix ptfe
+#output "plstring" {
+#  value = lookup(mongodbatlas_cluster.cluster-atlas.connection_strings[0].aws_private_link_srv, aws_vpc_endpoint.ptfe_service.id)
+#}
