88291ce155b9166b7826cac1add75d14fcde6e97
Michel Oosterhof <moosterhof@hashicorp.com>
2021-03-15 17:30:15 +0800

    update vars

--- provider.tf
+++ provider.tf
@@ -12,7 +12,7 @@ provider "mongodbatlas" {
 }
 
 provider "aws" {
-#  access_key = var.aws_access_key
-#  secret_key = var.aws_secret_key
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
   region     = var.aws_region
 }
