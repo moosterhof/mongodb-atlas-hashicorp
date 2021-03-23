f1bbcb6437485700cfdef448257e8a8225a2bd1c
Michel Oosterhof <moosterhof@hashicorp.com>
2021-03-23 19:41:18 +0800

    add aws introspection

---
+++ aws.tf
@@ -0,0 +1,7 @@
+data "aws_caller_identity" "current" {}
+
+locals {
+  account_id = data.aws_caller_identity.current.account_id
+  caller_arn = data.aws_caller_identity.current.arn
+  caller_user = data.aws_caller_identity.current.user_id
+}
